405ab58f3273676957da129001c6c78925cbd4c1
pavolbincik <pavolbinck@gmail.com>
2017-11-01 10:17:23 +0100

    added webpack + improve structure

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export function config() {
+    return {
+        "server": {
+//    "url": "http://127.0.0.1:3000",
+            "url": "https://raspberry-cams.herokuapp.com/"
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-/// <reference path="./rpio.d.ts" />
-
 import rpio = require("rpio");
-import socketIoClient = require("socket.io-client");
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io-client');
 
-const config = require("./config.json");
-const socket = socketIoClient(config.server.url);
+const config = require("./config.ts").config();
+const socket = io(config.server.url);
+console.log('Server url:', config.server.url);
 
 socket.on('connect', () => {
     console.log("Connected");
@@ -12,7 +13,7 @@ socket.on('connect', () => {
 
 socket.on('setPin', (data) => {
     console.log(`Relay ${data.relay} was switched ${data.state ? "ON" : "OFF"}`);
-    //rpio.write(1, rpio.HIGH);
+    rpio.write(1, rpio.HIGH);
 });
 
 socket.on('disconnect', () => {
