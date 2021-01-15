35fd997fcb13763500f55547b0fe35160a50f95b
pavolbincik <pavolbinck@gmail.com>
2021-01-15 09:52:17 +0100

    try to connect to pin 10 and listen to change

--- src/index.js
+++ src/index.js
@@ -16,17 +16,35 @@ const socket = io(config.server.url, {
   reconnectionAttempts: 99999
 });
 
+const ripSensorPin = 10;
+
 socket.on("connect", () => {
   console.log("Connected");
 
+  /*
+ * Configure the pin for input, and enable the internal pullup resistor which
+ * pulls the current high.  Pushing the button connects the pin to ground and
+ * pulls it low.  We poll for state changes and print a message when it does.
+ */
+  rpio.open(ripSensorPin, rpio.INPUT, rpio.PULL_UP);
+
+  rpio.poll(ripSensorPin, (cbpin) => {
+    socket.emit("pinChanged", {
+      pin: cbpin,
+      state: rpio.read(cbpin)
+    });
+    console.log("Button event on P%d (button currently %s)", cbpin, rpio.read(cbpin));
+  });
+
   socket.on("setPin", (data) => {
-    console.log('setPin', data);
-    rpio.write(1, rpio.HIGH);
+    console.log("setPin", data);
+    rpio.write(data.pin, data.state ? rpio.HIGH : rpio.LOW);
   })
 });
 
 socket.on("disconnect", () => {
   console.log("Disonnected");
+  rpio.close(ripSensorPin, rpio.INPUT);
 });
 
 socket.on("error", (err) => {
